952f9663c81425f9622b909baf3235e8e81624c3
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-14 13:11:23 -0700

    completes exercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,9 +5,9 @@ puts "----------"
 
 # Your code goes below here ...
 
-store_b = Store.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: true, womens_apparel: true)
-store_r = Store.create(name: "Richmond", annual_revenue: "1260000", womens_apparel: true)
-store_g = Store.create(name: "Gastown", annual_revenue: "190000", mens_apparel: true)
+Store.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: "1260000", womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: "190000", mens_apparel: true)
 
 puts Store.count
 pp Store.all
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,29 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
-puts "Exercise 4"
+puts "\nExercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: "224000", womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: "1900000", mens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# Sales for stores that carry men's apparel
+@mens_stores.each do |store|
+  puts "#{store.name}, Annual Revenue: #{store.annual_revenue}"
+end
+
+
+# Borrowing and modifying the code from Exercise one, create 3 more stores:
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+
+# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
+
+# Loop through each of these stores and output their name and annual revenue on each line.
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
