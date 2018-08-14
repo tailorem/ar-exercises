f5a3b2195aa3e226a9d3410d5d3193098859404d
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-14 13:14:50 -0700

    completes exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum "annual_revenue"
+pp "Company annual revenue: #{@total_revenue}"
+
+# Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
