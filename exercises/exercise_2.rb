42468cd55a93dbf1973fe95d0f8a6a41d4bf9c95
Taylour Em <29444471+tailorem@users.noreply.github.com>
2018-08-14 12:54:36 -0700

    completes exercise2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,18 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
+puts "\n\nExercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "East Van"
+
+pp @store1
+
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
